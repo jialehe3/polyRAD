0e940768852038281affb35a043f194853e457b5
何嘉乐 <hejiale@hejialedeMBP.home>
2019-03-29 00:00:25 -0500

    posterprob and evaluation

--- countsallele.R
+++ countsallele.R
@@ -43,6 +43,7 @@ names(locicountsum) <- rownames(locicountsum)
 
 threshold50000 <- locicountsum[which(locicountsum > 50000)]
 threshold1000mt2 <- mt2locisum[which(mt2locisum > locicountsum/1000)]
+
 thresholdunique <- unique(c(names(threshold1000mt2),names(threshold50000)))
 locicountWthresh <- locicountsum[which(is.na(match(names(locicountsum),thresholdunique)) == TRUE)]
 mt2locisumWthresh <- mt2locisum[which(is.na(match(names(locicountsum),thresholdunique)) == TRUE)]
@@ -51,14 +52,45 @@ mt2lociCWT50000 <- mt2locisum[which(is.na(match(names(locicountsum),names(thresh
 lociCWT1k <- locicountsum[which(is.na(match(names(locicountsum),names(threshold1000mt2))) == TRUE)]
 mt2lociCWT1k <- mt2locisum[which(is.na(match(names(locicountsum),names(threshold1000mt2))) == TRUE)]
 
+
 # no threshold used
-hist(log(mt2locisum/locicountsum),breaks = 50)
+hist(log(mt2locisum/locicountsum),breaks = 100,main = "origin")
 # 50k locisum threshold only
-hist(log(mt2lociCWT50000/lociCWT50000),breaks = 50)
+hist(log(mt2lociCWT50000/lociCWT50000),breaks = 100, main = "threshold_50000")
 # 1k mt2locisum threshold only
-hist(log(mt2lociCWT1k/lociCWT1k),breaks = 50)
-# 50k locisum and 1k mt2locisum thresholds apply
-hist(log(mt2locisumWthresh/locicountWthresh),breaks = 50)
+hist(log(mt2lociCWT1k/lociCWT1k),breaks = 100, main = "threshold_1k_mt2")
+# both 50k locisum and 1k mt2locisum thresholds apply
+hist(log(mt2locisumWthresh/locicountWthresh),breaks = 100, main = "both threshold")
 
 
 posterprobtest <- IterateHWE(myRAD[[1]])
+View(posterprobtest$posteriorProb[[1]][1,,][1:10])
+
+posterprob <- list()
+for (m in 1:length(myRAD)){
+  tryCatch({
+    # can pull allele frequency out of the iterate file: $alleleFreq
+    real_iterate <- IterateHWE(myRAD[[m]])
+    posterprob[[m]] <- real_iterate$posteriorProb
+  },error = function(e){cat("ERROR :",conditionMessage(e), "\n")} )
+}
+
+# posterprob[[1]][[1]][1,1:10,1:10] will show colnames and rownames
+
+PPequalzero <- list()
+for (n in 1:length(posterprob)) {
+  alleleloc <- myRAD[[n]]$alleles2loc
+  PPequalzero[[n]] <- sapply(1:nLoci(myRAD[[n]]),function(x)rowSums(posterprob[[n]][[1]][1,,alleleloc == x] < 0.1))
+  colnames(PPequalzero[[n]]) <- GetLoci(myRAD[[n]])
+}
+
+diff_alc_pp <- list()
+count <- 1
+for (o in 1:length(allelecount)) {
+  for (p in 1:nrow(allelecount[[o]])){
+    diff_alc_pp[[count]] <- sum(allelecount[[o]][p,] != PPequalzero[[o]][p,])
+    count <- count+1
+  }
+}
+
+# align posterprob with locicountsum and 
